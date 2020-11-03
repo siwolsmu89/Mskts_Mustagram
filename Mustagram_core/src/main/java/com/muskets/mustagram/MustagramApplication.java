0fccd0864097c78b152e4c76d7f75eccb523a10e
SiwolSmu89 <siwol_smuire89@naver.com>
2020-11-03 20:34:06 +0900

    Cleanup Directory structure
    ms.kwon, 2020/11/03

---
+++ Mustagram_core/src/main/java/com/muskets/mustagram/MustagramApplication.java
@@ -0,0 +1,13 @@
+package com.muskets.mustagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MustagramApplication.class, args);
+    }
+
+}
---
+++ Mustagram_core/src/test/java/com/muskets/mustagram/MustagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muskets.mustagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
