a6eb1bb48dd2b07d3a3131de16523b2cd1eddf5d
hyungtaek.ryu <bedmind10@gmail.com>
2020-11-26 20:06:40 +0900

    update JoinMember.js add call member/register.do
    20201126

--- mustagram_front/src/component/part/JoinMember.js
+++ mustagram_front/src/component/part/JoinMember.js
@@ -1,16 +1,45 @@
 import React, {Component} from "react";
 
 class JoinMember extends Component{
+    memberInsert = () => {
+        let id = document.getElementById("id").value;
+        let password = document.getElementById("password").value;
+        let name = document.getElementById("name").value;
+        let telephone = document.getElementById("telephone").value;
+        let email = document.getElementById("email").value;
+
+        let memberData = {
+            id : id,
+            password : password,
+            name : name,
+            telephone : telephone,
+            email : email
+        }
+
+        fetch('/member/register.do',{
+            method : 'POST',
+            body : JSON.stringify(memberData),
+            headers:{
+                'Content-Type' : 'application/json'
+            }
+        }).then(
+            res => res.text()
+        ).then(
+            res => console.log('response data : ',res)
+        );
+    }
     render() {
         return(
             <div>
                 <div class="login_field">
-                    아이디 : <input type="text" name="id"/> <br/>
-                    비밀번호 : <input type="text" name="password"/> <br/>
-                    비밀번호 확인 : <input type="text" name="password"/> <br/>
+                    아이디 : <input type="text" name="id" id="id"/> <br/>
+                    비밀번호 : <input type="text" name="password" id="password"/> <br/>
+                    이름 : <input type="text" name="name" id="name"/> <br/>
+                    전화번호 : <input type="text" name="telephone" id="telephone"/> <br/>
+                    이메일 : <input type="text" name="email" id="email"/> <br/>
                 </div>
                 <div class="submit_button">
-                    <button>
+                    <button onClick={this.memberInsert}>
                         가입
                     </button>
                 </div>
