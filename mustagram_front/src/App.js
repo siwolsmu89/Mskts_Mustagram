5119c9d7cc511512ec28fa890fc2e813086f312b
SiwolSmu89 <siwol_smuire89@naver.com>
2020-12-08 21:52:58 +0900

    Import Bootstrap
    2020/12/08

--- mustagram_front/src/App.js
+++ mustagram_front/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {BrowserRouter,Route} from 'react-router-dom'
 import MemberPageRouter from "./route/MemberPageRouter";
 import MainPageRouter from "./route/MainPageRouter";
+import 'bootstrap/dist/css/bootstrap.css'
 
 class App extends Component {
   render(){
