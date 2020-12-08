d5a14ba10504b483249df9d1399d2dc5ce93eeb7
SiwolSmu89 <siwol_smuire89@naver.com>
2020-12-08 21:23:53 +0900

    MyInfo.js - get datas from DB (Ajax) & create basic functions
    2020/12/08

--- mustagram_front/src/component/part/MyInfo.js
+++ mustagram_front/src/component/part/MyInfo.js
@@ -1,17 +1,39 @@
 import React, {Component} from 'react';
+import basicImage from '../../resources/profile/react-profile.png';
 
 class MyInfo extends Component {
 
+    state = {
+        myInfo:{}
+    }
+
     getMyInfo() {
         // ajax 요청으로 DB에서 사용자 정보 가져오기
+        fetch("/member/info.do?userId=siwolsmu")
+            .then(response => response.json())
+            .then(json => this.setState({myInfo:json}))
+    }
 
-
+    componentDidMount() {
+        this.getMyInfo();
+        console.log('myInfo', this.state.myInfo);
     }
 
     render() {
+        var myInfo = this.state.myInfo
+        var imageSrc = myInfo.image ? myInfo.image : basicImage;
+        var imageAlt = myInfo.image ? "Image fileName: " + myInfo.image + " - profile image of user " + myInfo.nickname : "Basic Profile";
         return (
             <div className="part part-wrapper part-my-info">
-                MyInfo
+                <div className="row">
+                    <div className="col-3 profile-image">
+                        <img src={imageSrc} alt={imageAlt}/>
+                    </div>
+                    <div className="col-9 profile-id-nickname">
+                        <h3 className="profile-id">{myInfo.id}</h3>
+                        <p className="profile-nickname">{myInfo.nickname}</p>
+                    </div>
+                </div>
             </div>
         );
     }
